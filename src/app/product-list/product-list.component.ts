53e9cb8cf979d87067b367db80e13e57a0249358
Gina B <gibi829@gmail.com>
2023-10-03 21:55:37 -0500

    Proyecto tienda-primera entrega

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = [...products];
+
+  share(nombre: string) {
+    window.alert('El producto '+ nombre+' se ha compartido');
+  }
+  onNotify(){
+    window.alert('Se le notificará cuando el producto salga a la venta')
+  }
+}
+
+
